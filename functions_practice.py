473575ec1b8e3b3f4b2ba7fb3de6c4cf420ce6c5
Christian Bell <camaribell@yahoo.com>
2023-08-01 17:05:56 -0700

    done 3

---
+++ functions_practice.py
@@ -0,0 +1,30 @@
+def hello():
+  print("Hello user!")
+
+hello()
+
+
+def pack(x, y, z):
+  return [x, y, z]
+
+print(pack(1, 2, 3))
+
+
+
+def eat_lunch(foods):
+  if len(foods) == 0:
+    print("My lunchbox is empty!") 
+  else:
+    print("First I eat " + foods[0])
+    for food in foods[1:]:
+      print("Next I eat " + food)
+
+eat_lunch(["sandwich", "apple", "cookie"])
+eat_lunch([])
+
+
+hello()
+print(pack(1, 2, 3))
+eat_lunch(["sandwich", "apple", "cookie"])
+eat_lunch([])
+eat_lunch(["sandwich"])
